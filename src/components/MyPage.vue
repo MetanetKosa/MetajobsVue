785a9339e62d7efb440b8834d60aec3ed5487a56
ssohee98 <jsohee98@naver.com>
2023-03-27 22:13:13 +0900

    Fix: updateMember

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -61,30 +61,46 @@
                         </div>
                         
                         <div class="d-flex flex-column flex-sm-row bg-light rounded-3 p-4 px-md-5">
-                            <form class="needs-validation" action="/member/update" method="post" novalidate>
-                                <input type="hidden" name="mem_id" value={{member.mem_id}}>
-                                <div class="mb-3">
-                                    <div class="input-group input-group-lg">
-                                    <input class="form-control date-picker rounded pe-5" type="text" id="reDate" name="mem_birth" placeholder="Choose date" data-datepicker-options="{&quot;altInput&quot;: true, &quot;altFormat&quot;: &quot;F j, Y&quot;, &quot;dateFormat&quot;: &quot;Y-m-d&quot;}"><i class="fi-calendar text-muted position-absolute top-50 end-0 translate-middle-y me-3"></i>
+                            <div class="row">
+                                <div class="col-10">
+                                    <br><br>
+                                    <h3 class="h4 mb-sm-4">회원정보 수정</h3>
+                                    <div class="d-flex flex-column  bg-light rounded-3 p-4 px-md-5" style="width:600px;">
+                                        <form @submit.prevent="update" role="form">
+                                            <input type="hidden" name="mem_id" value={{member.mem_id}}>
+                                            <div class="mb-3">
+                                                <label class="form-label" for="birth">생년월일 &nbsp;&nbsp;</label>
+                                                <input class="form-control date-picker rounded pe-5" type="text" id="reDate" v-model="state.form.mem_birth" placeholder="Choose date" data-datepicker-options="{&quot;altInput&quot;: true, &quot;altFormat&quot;: &quot;F j, Y&quot;, &quot;dateFormat&quot;: &quot;Y-m-d&quot;}"><i class="fi-calendar text-muted position-absolute top-50 end-0 translate-middle-y me-3"></i>
+                                            </div><br>
+                                            <div class="mb-3">
+                                                <label class="form-label" for="phone">핸드폰번호 &nbsp;&nbsp;</label>
+                                                <input class="form-control" type="text" id="phone" v-model="state.form.mem_phone" placeholder="회원 핸드폰" required>
+                                            </div><br>
+                                            <div class="mb-3">
+                                                <label class="form-label" for="address">주소 &nbsp;&nbsp;</label>
+                                                <input class="form-control" type="text" id="address" v-model="state.form.mem_address" placeholder="회원 주소" required>
+                                            </div><br>
+                                            <div class="mb-3">
+                                                <label class="form-label" for="email">Email &nbsp;&nbsp;</label>
+                                                <input class="form-control" type="text" id="email" v-model="state.form.mem_email" placeholder="회원 email" required>
+                                            </div>
+                                            <div style="margin-left:650px;">                             
+                                                <button class="btn btn-primary btn-lg rounded-pill ms-sm-auto" type="submit">정보 수정</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <button class="btn btn-primary btn-lg rounded-pill ms-sm-auto" type="button" data-oper=''>회원 탈퇴</button>
+                                            </div>
+                                        </form>
                                     </div>
-                                </div> 
-                                <div class="mb-3">
-                                    <label class="form-label" for="phone">핸드폰번호 &nbsp;&nbsp;</label>
-                                    <input class="form-control" type="text" id="phone" name="mem_phone" placeholder="회원 핸드폰"/> required>
-                                </div> 
-                                <div class="mb-3">
-                                    <label class="form-label" for="address">주소 &nbsp;&nbsp;</label>
-                                    <input class="form-control" type="text" id="address" name="mem_address" placeholder="회원 주소"/>" required>
                                 </div>
-                                <div class="mb-3">
-                                    <label class="form-label" for="email">Email &nbsp;&nbsp;</label>
-                                    <input class="form-control" type="text" id="email" name="mem_email" placeholder="회원 email"/>" required>
-                                </div>
-                                <button class="btn btn-primary d-block w-100 mb-4" type="submit">회원정보 수정</button>
-                            </form>
+                            </div>
+                            
                         </div>
-                        <button class="btn btn-primary btn-lg rounded-pill ms-sm-auto" type="button" data-oper=''>수정</button>
-                    </div>
+                        <!-- <div class="d-flex-end flex-column flex-sm-row bg-light rounded-3 p-4 px-md-5">
+                            <div style="margin-left:650px;">                             
+                                <button class="btn btn-primary btn-lg rounded-pill ms-sm-auto" type="submit">정보 수정</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <button class="btn btn-primary btn-lg rounded-pill ms-sm-auto" type="button" data-oper=''>회원 탈퇴</button>
+                            </div>
+                        </div> -->
+                    </div>                           
                 </div>
             </div>
     </main>
@@ -93,8 +109,9 @@
 
 <script>
 import { useRouter } from 'vue-router';
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 import axios from 'axios';
+
 export default {
 
     // props: {
@@ -120,19 +137,45 @@ export default {
     // setup(prop) {
     //     const router = useRouter();
     // }
+
     setup() {
 
     const member = ref([]);
 
-     
     axios.get('api/users/member11').then(({data}) => {
         member.value = data;
         console.log(member);
     })
 
+    const router = useRouter();
+
+    const state = reactive({
+        members: [],
+        form: {
+            mem_id: "member11",
+            mem_birth: '',
+            mem_phone: '',
+            mem_address: '',
+            mem_email: ''
+        }
+    })
+
+    const update = () => {
+        const args = JSON.parse(JSON.stringify(state.form));
+        args.members = JSON.stringify(state.members);
+        console.log(args.members);
+        axios.put('/api/users/member11', args).then(() => {
+            alert("회원정보 수정 완료!");
+            router.push({path: "/"})
+        })
+    }
+
+
 
     return {
         member,
+        state,
+        update,
     }
     
 }
