ca9e85fa563b8528d7c55cd953739e9e173cd831
jseunn <sesej4512@gmail.com>
2023-03-27 13:40:23 +0900

    Feat: DB Connection using axios

--- src/pages/recruit/review.vue
+++ src/pages/recruit/review.vue
@@ -86,55 +86,11 @@
                         </div>
                     </div>
                 </div>
-                <div v-if="!reviews.length">
+                <!-- <div v-if="!reviews.length">
                     검색 결과가 없습니다.
-                </div>
-
+                </div> -->
                 <div style="min-height:300px;">
-                    <c:forEach items="${list}" var="review">
-                        <div class="mb-2 pb-4 border-bottom">
-                            <div class="d-flex justify-content-between mb-3">
-                                <div class="d-flex align-items-center pe-2"><img class="rounded-circle me-1" src="${path}/resources/img/avatars/13.png" width="48" alt="Avatar">
-                                    <div class="ps-2">
-                                        <!-- 변경된 부분 -->
-                                        <h6 class="fs-base mb-0">
-                                            <c:if test="${review.cno==1234}">메타넷디지털</c:if>
-                                            <c:if test="${review.cno==1235}">대한기업</c:if>
-                                            <c:if test="${review.cno==1236}">한국유통</c:if>
-
-                                        </h6>
-
-                                        <!-- 여기까지 -->
-                                        <h6 class="fs-sm mb-0">${review.reJob}</h6>
-                                    </div>
-                                </div>
-                                <div>
-                                    <span class="fs-sm"><fmt:formatDate pattern="yyyy년 MM월 dd일" value="${review.reDate}"/></span>
-                                        <form id="deleteForm" action="${path}/review/delete" method="post">
-                                            <input type="hidden" name="rno" value="${review.rno}">
-                                            <span><button class="btn btn-primary" type="submit">삭제</button></span>
-                                        </form>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-2">
-                                    <h6 class="fs-xs mb-0">면접일</h6>
-                                    <h6 class="fs-xs fw-light mb-0">
-                                        <fmt:formatDate pattern="yyyy년 MM월 dd일" value="${review.reDate}" />
-                                    </h6>
-                                    <h6 class="fs-xs pt-2 mb-0">면접인원</h6>
-                                    <h6 class="fs-xs fw-light mb-0">${review.pnum}</h6>
-                                </div>
-                                <div class="col-10">
-                                    <h3 class="fs-sm mb-0">면접내용</h3>
-                                    <p class="fs-sm" style="min-height:60px;">${review.reContent}</p>
-                                    <h3 class="fs-sm mb-0">면접 결과</h3>
-                                    <p class="fs-sm">${review.reResult}</p>
-
-                                </div>
-                            </div>
-                        </div>
-                    </c:forEach>
+                    <ReviewList :reviews="reviews"/>
                 </div>
               
             </div>
@@ -164,12 +120,15 @@
 </template>
 
 <script>
+import {ref} from 'vue';
     import axios from "axios";
-    import { async } from "rxjs";
+    import ReviewList from '@/components/ReviewList.vue';
     import { useRouter } from 'vue-router';
 
 export default {
-
+    components: {
+        ReviewList
+    },
 
     setup() {
         const reviews = ref([]);
@@ -181,23 +140,28 @@ export default {
         });
       }
       
-      
-      return{
-          moveToCreatePage
-      }
-
       const getReviews = async () => {
         try {
             const res = await axios.get(
-                "http://localhost:8082/todos"
+                '/reviews'
                 );
-            
+                console.log(res.data);
                 reviews.value = res.data;
             
         } catch (error) {
             console.log(error);
         }
       }
+
+      getReviews();
+      
+      return{
+          moveToCreatePage,
+          reviews,
+             getReviews
+      }
+
+
     }
 
 }
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false
+    transpileDependencies: true,
+    lintOnSave: false
 })
 
-// module.exports = {
-//   devServer: {
-//     proxy: "http://localhost:8082"
-//   }
-// }
+module.exports = {
+    devServer: {
+        proxy: 'http://localhost:8082' // Spring boot의 주소 및 포트
+    }
+}
